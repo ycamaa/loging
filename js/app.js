02ceb3b08991d262fc137eca9dba37fa2268ac05
o.jannane <32781185+ycamaa@users.noreply.github.com>
2019-10-29 19:34:24 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 function myFunction() {
     document.getElementById('id01').style.display = 'block';
+    document.getElementById('id01').style.animation = "animatezoom 0.5s";
     document.getElementById('logocent').style.gridColumnGap = '425px';
+    document.getElementById('logocent').style.animation = "move 0.5s";
 }
 
 function myFunction1() {
     document.getElementById('id01').style.display = 'none';
+    document.getElementById('id01').style.animation = "animatezoomback 0.5s";
     document.getElementById('logocent').style.gridColumnGap = '0px';
+    document.getElementById('logocent').style.animation = "back 0.5s";
 }
