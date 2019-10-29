0be561d31633ee317e3cbf0910532a1383092b47
o.jannane <32781185+ycamaa@users.noreply.github.com>
2019-10-29 19:09:55 +0100

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById('id01').style.display = 'block';
+    document.getElementById('logocent').style.gridColumnGap = '425px';
+}
+
+function myFunction1() {
+    document.getElementById('id01').style.display = 'none';
+    document.getElementById('logocent').style.gridColumnGap = '0px';
+}
